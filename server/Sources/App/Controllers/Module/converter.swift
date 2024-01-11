28ae11ee8efd43c9b4f543acdcb6091a88098709
LimeFlare <113850109+LimeFlare@users.noreply.github.com>
2024-01-11 11:11:33 +0800

    add commands

--- server/Sources/App/Controllers/Module/converter.swift
+++ server/Sources/App/Controllers/Module/converter.swift
@@ -27,4 +27,4 @@ extension SurgeController {
                 let modifierContents = modifierResponses.compactMap { $0.body.flatMap { String(buffer: $0) } }
 
                 guard modifierContents.count > 0 else {
-           
+                    return req.eventLoop.future(error: Abort(.badRequest, reason: "no content available
