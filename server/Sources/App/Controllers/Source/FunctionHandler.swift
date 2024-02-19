04dcdefd758b9ddbba1856b7c061469e75fa9750
LimeFlare <113850109+LimeFlare@users.noreply.github.com>
2024-02-19 12:16:41 +0800

    update package files

--- server/Sources/App/Controllers/Source/FunctionHandler.swift
+++ server/Sources/App/Controllers/Source/FunctionHandler.swift
@@ -55,3 +55,40 @@ fileprivate enum GroupFilterFunction: FunctionType {
             case "prefix": self = .prefix
             case "suffix": self = .suffix
             default: return nil
+            }
+        }
+
+        func operate(withOperand operand: String, target: String) -> Bool {
+            guard !operand.isEmpty else { return true }
+            switch self {
+            case .contains: return target.lowercased().contains(operand.lowercased())
+            case .matches: return target.range(of: operand, options: [.regularExpression, .caseInsensitive], range: nil, locale: nil) != nil
+            case .prefix: return target.lowercased().hasPrefix(operand.lowercased())
+            case .suffix: return target.lowercased().hasSuffix(operand.lowercased())
+            }
+        }
+    }
+
+    static let matchingRegex = try! NSRegularExpression(pattern: #"\$group\(\s*'([^'']+)'\s*,\s*(contains?|match(es)?|prefix|suffix)\s*,\s*'(([^']|(\\'))*)'\s*\)$"#, options: [.anchorsMatchLines])
+
+    static func handle(function: NSString, matchingResult: NSTextCheckingResult, referenceModifiers: [Surge.GroupModifier]) -> String {
+        let designatedModifierName = function.substring(with: matchingResult.range(at: 1))
+        guard let referencedModifier = referenceModifiers.first(where: { $0.name == designatedModifierName }) else {
+            return function as String
+        }
+
+        guard let op = Operator(rawValue: function.substring(with: matchingResult.range(at: 2))) else {
+            return function as String
+        }
+
+        let operand = function.substring(with: matchingResult.range(at: 4)).replacingOccurrences(of: #"\'"#, with: "'").replacingOccurrences(of: #"\""#, with: "\"")
+
+        let matchedKeys = (referencedModifier.insertedModifiers + referencedModifier.appendedModifiers).compactMap { modifier -> String? in
+            guard case .keyValue(let kv) = modifier else {
+                return nil
+            }
+            return op.operate(withOperand: operand, target: kv.key) ? kv.key : nil
+        }
+        return matchedKeys.joined(separator: ", ")
+    }
+}
---
+++ server/Sources/App/Controllers/Source/Group.swift
@@ -0,0 +1,5 @@
+//
+//  Group.swift
+//  SSTest
+//
+//  Created by 
