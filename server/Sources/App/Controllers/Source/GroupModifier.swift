c4cf786fd04ddd976cfe629f5987da15c8abaad0
LimeFlare <113850109+LimeFlare@users.noreply.github.com>
2024-08-10 17:57:48 +0800

    Update functional tests

--- server/Sources/App/Controllers/Source/GroupModifier.swift
+++ server/Sources/App/Controllers/Source/GroupModifier.swift
@@ -209,4 +209,4 @@ extension Surge.GroupModifier {
             toHandledContents = resource
         }
 
- 
+        let toHandledLines = toHandledContents.trimmingCharacters(in: .whitespacesAndNewl
