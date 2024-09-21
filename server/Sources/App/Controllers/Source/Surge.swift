117a9116d2811613f907d455583e96dcd057746a
LimeFlare <113850109+LimeFlare@users.noreply.github.com>
2024-09-21 16:54:23 +0800

    Remove logging statements

--- server/Sources/App/Controllers/Source/Surge.swift
+++ server/Sources/App/Controllers/Source/Surge.swift
@@ -38,3 +38,66 @@ extension Surge {
         }
 
         typealias GroupName = String
+
+        var sortedGroupNames: [String] = []
+        var groups: [GroupName: [GroupModifier.Modifier]] = [:]
+        var update: [(GroupName, [GroupModifier.Updator])] = []
+
+        // 3. Merge all modify and replace group's modifiers.
+        filtedGroupModifiers.forEach { groupModifier in
+            if !groupModifier.updators.isEmpty {
+                update.append((groupModifier.groupName, groupModifier.updators))
+            }
+            switch groupModifier.modificationType {
+            case .modify:
+                if var modifiers = groups[groupModifier.groupName] {
+                    modifiers.insert(contentsOf: groupModifier.insertedModifiers, at: 0)
+                    modifiers.append(contentsOf: groupModifier.appendedModifiers)
+                    groups[groupModifier.groupName] = modifiers
+                } else {
+                    fallthrough
+                }
+            case .replace:
+                groups[groupModifier.groupName] = groupModifier.insertedModifiers + groupModifier.appendedModifiers
+                sortedGroupNames.append(groupModifier.groupName)
+            }
+        }
+
+        // 4. Execute all updators.
+        update.forEach { groupName, updators in
+            guard var modifiers = groups[groupName] else {
+                return
+            }
+            updators.forEach { updator in
+                guard let firstMatchedModifierIndex = modifiers.firstIndex(where: { modifier -> Bool in
+                    if case .keyValue(let kv) = modifier,
+                        kv.key == updator.designatedKey {
+                        return true
+                    } else {
+                        return false
+                    }
+                }) else {
+                    return
+                }
+
+                modifiers[firstMatchedModifierIndex] = updator.update(modifier: modifiers[firstMatchedModifierIndex])
+            }
+            groups[groupName] = modifiers
+        }
+
+        // 5. Handle functions (replacing groups)
+        return groups.mapValues { modifiers -> [GroupModifier.Modifier] in
+            return modifiers.map { modifier in
+                guard case .keyValue(let kv) = modifier,
+                    !kv.functionIndics.isEmpty else {
+                        return modifier
+                }
+                return .keyValue(kv.handleFunction(withReferenceModifiers: filtedGroupModifiers))
+            }
+        }.sorted(by: {
+            sortedGroupNames.firstIndex(of: $0.key)! < sortedGroupNames.firstIndex(of: $1.key)!
+        }).map { groupName, modifiers -> String in
+            return "[\(groupName)]\n" + modifiers.map { $0.exportToString() }.joined(separator: "\n") + "\n"
+        }.joined(separator: "\n")
+    }
+}
